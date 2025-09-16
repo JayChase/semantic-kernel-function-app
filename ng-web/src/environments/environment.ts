43827c83b6d6aa0b4fa524fea91d9e33e58d1249
JayChase <JonathanChase@outlook.com>
2025-09-16 15:45:48 +0900

    🧹

--- ng-web/src/environments/environment.ts
+++ ng-web/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   aiApi: {
-    uri: 'https://func-cdnkoyn6tkxdw.azurewebsites.net',
+    uri: 'https://func-token.azurewebsites.net',
     getKeyCommand:
-      'az login --tenant 8e8dee8b-d10c-4894-8d43-cae3f9ed549b && az functionapp function keys list --resource-group rg-clone1 --name func-cdnkoyn6tkxdw --function-name chat',
+      'az login --tenant tenant-id && az functionapp function keys list --resource-group rg-env-or-token --name func-token --function-name chat',
     endpoints: {
       chat: 'api/chat'
     }
